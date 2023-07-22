d17f28e95dfb4a2f4fbebcbfb3725781e7172834
Syed Usman Zafar <65255393+syedusmanzafar@users.noreply.github.com>
2023-07-22 18:36:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbydtcsbVAoPNcrmIwMmNnOTmhXdDJ0sp2usnoO486TJiXLwXuoutkawJr29997OOj2eCg/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwgsFrxbXWu9QY0ZuxDQRauJrwYInl_jS8k2NICCwLfKRibqrsZyiCADJ69DubC7Slu/exec";
