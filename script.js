53144e0987560c1838bb3c914e6bc0f1f28a48e1
Syed Usman Zafar <65255393+syedusmanzafar@users.noreply.github.com>
2023-07-23 14:56:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
 var qrcode = new QRCode("qrcode");
 var url = document.location.href;
-console.log(url);
-var k = url.lastIndexOf("k=")>0 ? url.substr(url.lastIndexOf("k=")+2) : "";
+console.log("Current URL:", url);
+var k = url.lastIndexOf("k=") > 0 ? url.substr(url.lastIndexOf("k=") + 2) : "";
 
-if(k){
-  console.log(k);
+if (k) {
+  console.log("Google Calendar ID (k parameter):", k);
   $(".lds-ring").show();
-  fetch(AS_URL_BASE+'?k='+k)
-  .then(r => r.text())
-  .then((r) => {
-    console.log(r);
-    //document.write(r);
-    if(r){
-      document.location.replace(r);
-    }
-  })
-  .catch(err => console.log(err))
-
-}
-else
-{
+  fetch(AS_URL_BASE + '?k=' + k)
+    .then(r => r.text())
+    .then((r) => {
+      console.log("Fetched YouTube link from Google Apps Script:", r);
+      // document.write(r);
+      if (r) {
+        document.location.replace(r);
+      }
+    })
+    .catch(err => console.log("Error fetching YouTube link:", err));
+} else {
   // si no hay clave, mostrar generador de QR
   $("#urlWS").text(AS_URL_BASE);
   $("#qrEdit").show();
 
   function makeCode() {
     var calId = $("#calId").val();
-    if(calId){
-      var qrTargetUrl=url+"?k="+encodeURI(calId);
+    if (calId) {
+      var qrTargetUrl = url + "?k=" + encodeURI(calId);
       $("#urlQR").text(qrTargetUrl);
-      $("#urlQR").attr("href",qrTargetUrl);
+      $("#urlQR").attr("href", qrTargetUrl);
       qrcode.makeCode(qrTargetUrl);
     }
   }
 
   makeCode();
 
-  $("#calId").
-    on("blur", function () {
+  $("#calId")
+    .on("blur", function () {
       makeCode();
-    }).
-    on("keydown", function (e) {
+    })
+    .on("keydown", function (e) {
       if (e.keyCode == 13) {
         makeCode();
       }
     });
 }
-
